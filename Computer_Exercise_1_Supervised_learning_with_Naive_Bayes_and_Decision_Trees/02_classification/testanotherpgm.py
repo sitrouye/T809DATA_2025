c19554de763c775cd8ad617f3a4e95d5786470e0
sitrouye <best.elvire@gmail.com>
2025-08-28 21:43:08 +0200

    answer section 8

--- Computer_Exercise_1_Supervised_learning_with_Naive_Bayes_and_Decision_Trees/02_classification/testanotherpgm.py
+++ Computer_Exercise_1_Supervised_learning_with_Naive_Bayes_and_Decision_Trees/02_classification/testanotherpgm.py
@@ -15,14 +15,20 @@ print(norm.rvs(0,1))
 print(gen_data(2,[0,2], [4,4]))
 
 
-features, targets, classes = gen_data(50,[-1,1], [np.sqrt(5), np.sqrt(5)])
+features, targets, classes = gen_data(50,[-4,4], [np.sqrt(2), np.sqrt(2)])
 print(features)
 print(features.shape[0])
 
-
-
-plt.scatter(features,[0 for i in range (len(features))])
-# plt.show()
+class_feat = [[]for i in range(len(classes))]
+for i in range(len(features)):
+    for j in range(len(classes)):
+        if targets[i] == classes[j] :
+            class_feat[j].append(features[i])
+color = ['b','r','g','k','m', 'c']
+marker = ['o','x']
+for j in range(len(classes)):
+    plt.scatter(class_feat[j],[0 for i in range (len(class_feat[j]))], c = color[j], marker = marker[j] )
+plt.show()
 
 
 
@@ -32,4 +38,39 @@ class_cov = covar_of_class(train_features, train_targets, 0)
 print(class_mean, np.sqrt(class_cov))
 print(-1, np.sqrt(5))
 like = maximum_likelihood(train_features, train_targets, test_features, classes)
-print(predict(like))
+
+l =predict(like)
+acc = 0
+for i in range(len(l)) :
+    if test_targets[i] == l[i]:
+        acc +=1
+acc = acc/len(l)
+print(l)
+print('accuracy for dataset 2', acc)
+
+
+
+
+
+
+
+
+features, targets, classes = gen_data(50,[-1,1], [np.sqrt(5), np.sqrt(5)])
+
+
+(train_features, train_targets), (test_features, test_targets) = split_train_test(features, targets, 0.8)
+class_mean = mean_of_class(train_features, train_targets, 0)
+class_cov = covar_of_class(train_features, train_targets, 0)
+print(class_mean, np.sqrt(class_cov))
+print(-1, np.sqrt(5))
+like = maximum_likelihood(train_features, train_targets, test_features, classes)
+
+l =predict(like)
+acc_1 = 0
+for i in range(len(l)) :
+    if test_targets[i] == l[i]:
+        acc_1 +=1
+acc_1 = acc_1/len(l)
+print(l)
+print('accuracy for dataset 1', acc_1)
+
